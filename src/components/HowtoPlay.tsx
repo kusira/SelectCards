2c560a5fcc1bfcb4734670040e04d80913459202
kusira <takaaki200302@gmail.com>
2024-01-03 16:42:54 +0900

    遊び方の説明の修正

--- src/components/HowtoPlay.tsx
+++ src/components/HowtoPlay.tsx
@@ -3,12 +3,13 @@
 function HowtoPlay({ setScene }: { setScene: any }) {
   return (
     <div className="howtoplay-UI UI absolute w-10/12">
-      <p className="leading-8 mb-2">
+      <p className="mb-2">
         カードを複数枚選んでそれらの合計値を目標値に一致させます。<br />
+        (対象のカードをクリックをすることで選択/キャンセルをすることができます。)<br /><br />
         制限時間(60秒)内に解いた問題数がスコアとなります。<br />
         ノーミスの場合は制限時間が伸びます。
       </p>
-      <img src="./img/how-to-play.png" width={240} className="mx-auto mb-2"/>
+      <img src="./img/how-to-play.png" className="how-to-play mx-auto mb-2"/>
       <div
         className="w-max mx-auto bg-green-400 hover:bg-green-600 rounded-md p-3 shadow-gray-400 shadow-sm cursor-pointer transition-all"
         onClick={() => setScene(0)}
